d5f48294efae3a611a13f518381703f480299c88
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-09-24 12:09:12 -0500

    add routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,22 +8,38 @@ import CommentPage from '../CommentPage/CommentPage';
 import ReviewPage from '../ReviewPage/ReviewPage';
 import ConfirmationPage from '../ConfirmationPage/ConfirmationPage';
 
+// import Router
+import { useDispatch, useSelector } from 'react-redux';
+import { HashRouter as Router, Route, Link} from 'react-router-dom';
+
 function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-      
-      <FeelingPage />
-      <UnderstandingPage />
-      <SupportedPage />
-      <CommentPage />
-      <ReviewPage />
-      <ConfirmationPage />
-
+      <Router>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        <Route exact path="/">
+          <FeelingPage />
+        </Route>
+        <Route exact path="/QuestionTwo">
+          <UnderstandingPage />
+        </Route>
+        <Route exact path="/QuestionThree">
+          <SupportedPage />
+        </Route>
+        <Route exact path="/QuestionFour">
+          <CommentPage />
+        </Route>
+        <Route exact path="/Review">
+          <ReviewPage />
+        </Route>
+        <Route exact path="/Confirm">
+          <ConfirmationPage />
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -1,8 +1,33 @@
+import {useState} from 'react';
+import {useSelector} from 'react-redux';
 
 function ReviewPage() {
 
+    // const feeling = useSelector(selector => selector.feelReducer);
+    // const understanding = useSelector(selector => selector.understandReducer);
+    // const support = useSelector(selector => selector.supportReducer);
+    // const comments = useSelector(selector => selector.commentReducer);
+
+    // console.log(`feeling: ${feeling}, understanding: ${understanding}, support: ${support}, comments: ${comments}`);
+    // const [responses, setResponses] = useState({
+    //         feeling,
+    //         understanding,
+    //         support,
+    //         comments
+    // })
+
     return (
-        <p>Here is the ReviewPage</p>
+        <>
+        {/* <p>Here is the ReviewPage</p>
+        <p>How are you feeling today?</p>
+        <p>{feeling}/5</p>
+        <p>How well are you understanding the content?</p>
+        <p>{understanding}/5</p>
+        <p>How well are you being supported?</p>
+        <p>{support}/5</p>
+        <p>Any comments you want to leave?</p>
+        <p>{comments}/5</p> */}
+        </>
     )
 }
 
