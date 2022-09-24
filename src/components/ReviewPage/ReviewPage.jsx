64b4796420ad5b011575588dddfaa5ac6e5e8374
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-09-24 15:51:05 -0500

    post route working

--- src/components/ReviewPage/ReviewPage.jsx
+++ src/components/ReviewPage/ReviewPage.jsx
@@ -4,44 +4,53 @@ import { useHistory } from 'react-router-dom';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import axios from 'axios';
 
 
 function ReviewPage() {
     const history = useHistory();
-
+    
+    // pull in reducers
     const feeling = useSelector(store => store.feelReducer);
     const understanding = useSelector(store => store.understandReducer);
     const support = useSelector(store => store.supportReducer);
     const comments = useSelector(store => store.commentReducer);
+    
+    // put reducers in an object
+    const [feedbackObject, setfeedbackObject] = useState({
+        feeling: feeling,
+        understanding: understanding,
+        support: support,
+        comments: comments
+    });
 
-    // console.log(`feeling: ${feeling}, understanding: ${understanding}, support: ${support}, comments: ${comments}`);
-    const [responses, setResponses] = useState({
-            feeling,
-            understanding,
-            support,
-            comments
-    })
-
-    const handleNext = event => {
-        event.preventDefault();
-
-        // TODO: POST TO DATABASE
-        history.push("/Confirm")
+    const handleNext = () => {
+        axios({
+            method: 'POST',
+            url: '/feedback',
+            data: feedbackObject
+        }).then(response => {
+            console.log('POST successful!');
+            history.push("/Confirm")
+        }).catch(error => {
+            console.log('Feedback not submitted due to:', error);
+            alert('Feedback submission error. Try again later.');
+        });
     }
 
     return (
         <>
         <Typography component="legend">How are you feeling today?</Typography>
-        <Rating name="read-only" value={responses.feeling} readOnly />
+        <Rating name="read-only" value={feedbackObject.feeling} readOnly />
 
         <Typography component="legend">How well are you understanding the content?</Typography>
-        <Rating name="read-only" value={responses.understanding} readOnly />
+        <Rating name="read-only" value={feedbackObject.understanding} readOnly />
 
         <Typography component="legend">How well are you being supported?</Typography>
-        <Rating name="read-only" value={responses.support} readOnly />
+        <Rating name="read-only" value={feedbackObject.support} readOnly />
 
         <p>Any comments you want to leave?</p>
-        <p>{responses.comments}</p>
+        <p>{feedbackObject.comments}</p>
         
         <Button onClick={handleNext} variant="contained">Submit</Button>
         </>
