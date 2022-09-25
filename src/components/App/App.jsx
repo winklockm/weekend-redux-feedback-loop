7ea3fc58940d2693e85720fb4727e32f1e909bd2
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-09-25 12:18:15 -0500

    change title

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,8 +18,7 @@ function App() {
     <div className='App'>
       <Router>
         <header className='App-header'>
-          <h1 className='App-title'>Feedback!</h1>
-          <h4>Don't forget it!</h4>
+          <h1 className='App-title'>Feedback Survey</h1>
         </header>
         <Route exact path="/">
           <FeelingPage />
